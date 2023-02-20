43544c4226be78503320457498bf187c5c8dd04a
ryoum-0930 <dara.ryouma@gmail.com>
2023-02-20 18:30:19 +0900

    [add] testpage and button

---
+++ frontend/src/components/TestPage.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+    <v-btn class="ma-2" color="primary">Upload</v-btn>
+    <v-icon size="large" color="green-darken-2"> mdi-domain </v-icon>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+
+  data() {
+    return {
+      ecosystem: [
+        {
+          text: "vuetify-loader",
+          href: "https://github.com/vuetifyjs/vuetify-loader/tree/next",
+        },
+        {
+          text: "github",
+          href: "https://github.com/vuetifyjs/vuetify/tree/next",
+        },
+        {
+          text: "awesome-vuetify",
+          href: "https://github.com/vuetifyjs/awesome-vuetify",
+        },
+      ],
+      importantLinks: [
+        {
+          text: "Chat",
+          href: "https://community.vuetifyjs.com",
+        },
+        {
+          text: "Made with Vuetify",
+          href: "https://madewithvuejs.com/vuetify",
+        },
+        {
+          text: "Twitter",
+          href: "https://twitter.com/vuetifyjs",
+        },
+        {
+          text: "Articles",
+          href: "https://medium.com/vuetify",
+        },
+      ],
+      whatsNext: [
+        {
+          text: "Explore components",
+          href: "https://vuetifyjs.com",
+        },
+        {
+          text: "Roadmap",
+          href: "https://vuetifyjs.com/en/introduction/roadmap/",
+        },
+        {
+          text: "Frequently Asked Questions",
+          href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+        },
+      ],
+    };
+  },
+});
+</script>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Click Test</h2>
   </div>
+  <test-page />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import TestPage from "@/components/TestPage.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    TestPage,
   },
 })
 export default class HomeView extends Vue {}
