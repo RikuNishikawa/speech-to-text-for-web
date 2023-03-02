89f7ad5d1882b9a8fecb29d9c32fc10b781aa306
りく <nishi.riku09@gmail.com>
2023-03-02 18:29:56 +0900

    [add] 入力ページに画像のコンポーネントを挿入

---
+++ frontend/src/components/HeaderMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-img :width="1400" aspect-ratio="16/9" cover src="../assets/headerpage.png"
+    >音声→テキスト</v-img
+  >
+</template>
+<script lang="ts">
+import { Options, Vue } from "vue-property-decorator";
+
+@Options({})
+export default class TestPage extends Vue {
+  private extend = [".wav", ".mp3"];
+  private voiceFile = "";
+  public msg = "";
+  private onClick() {
+    alert(this.voiceFile);
+  }
+}
+</script>
