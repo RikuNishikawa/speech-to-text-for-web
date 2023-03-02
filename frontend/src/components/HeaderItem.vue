8d855a0bf0b1cc1f95f427696068e6904ec3ad6d
ryoum-0930 <dara.ryouma@gmail.com>
2023-03-02 15:39:26 +0900

    [add] ヘッダーのページ遷移の実装

--- frontend/src/components/HeaderItem.vue
+++ frontend/src/components/HeaderItem.vue
@@ -2,11 +2,18 @@
   <v-container>
     <v-app-bar flat class="px-16">
       <v-toolbar-title class="header-title">VCT</v-toolbar-title>
-      <div class="">
-        <v-btn variant="text" class="header-btn"> Home </v-btn>
-        <v-btn variant="text" class="header-btn"> 音声テキスト変換 </v-btn>
-        <v-btn variant="text" class="header-btn"> 開発メンバー </v-btn>
-      </div>
+      <v-spacer />
+      <v-toolbar-items>
+        <v-btn variant="text" class="header-btn" @click="moveHome">
+          Home
+        </v-btn>
+        <v-btn variant="text" class="header-btn" @click="moveInput">
+          音声テキスト変換
+        </v-btn>
+        <v-btn variant="text" class="header-btn" @click="moveMember">
+          開発メンバー
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
   </v-container>
 </template>
@@ -15,7 +22,17 @@
 import { Options, Vue } from "vue-property-decorator";
 
 @Options({})
-export default class HeaderItem extends Vue {}
+export default class HeaderItem extends Vue {
+  private moveHome() {
+    this.$router.push({ path: "/" });
+  }
+  private moveInput() {
+    this.$router.push({ path: "/input" });
+  }
+  private moveMember() {
+    this.$router.push({ path: "/member" });
+  }
+}
 </script>
 <style>
 .header-title {
