411b3e5fc9cd5f28e75c0e77c91beeefb5c6e636
りく <nishi.riku09@gmail.com>
2023-03-02 18:37:23 +0900

    [add] HeaderMainのコンポーネントの呼び出し

--- frontend/src/views/InputView.vue
+++ frontend/src/views/InputView.vue
@@ -1,14 +1,20 @@
 <template>
-  <div>入力ページ</div>
+  <v-app>
+    <v-main>
+      <header-main />
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-property-decorator";
 import TestPage from "@/components/TestPage.vue"; // @ is an alias to /src
+import HeaderMain from "@/components/HeaderMain.vue"; // @ is an alias to /src
 
 @Options({
   components: {
     TestPage,
+    HeaderMain,
   },
 })
 export default class InputView extends Vue {}
