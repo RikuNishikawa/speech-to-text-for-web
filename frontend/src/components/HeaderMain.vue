06504a7d3c640d9ad4c3c877d5555bc9414b4779
りく <nishi.riku09@gmail.com>
2023-03-02 23:38:55 +0900

    [change] 文字の太さ変更

--- frontend/src/components/HeaderMain.vue
+++ frontend/src/components/HeaderMain.vue
@@ -1,10 +1,10 @@
 <template>
   <v-img :width="1400" aspect-ratio="16/9" cover src="../assets/headerpage.png"
     ><div class="header-text">
-      <p class="header-maintext font-weight-black text-white">
+      <p class="header-maintext font-weight-bold text-white">
         音&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;声&nbsp;&nbsp;&nbsp;&nbsp;→&nbsp;&nbsp;&nbsp;&nbsp;テ&nbsp;キ&nbsp;ス&nbsp;ト
       </p>
-      <p class="header-subtext text-white">
+      <p class="header-subtext font-weight-normal text-white">
         \音声データを入れて、テキストデータに変換しよう！/
       </p>
     </div></v-img
@@ -27,6 +27,7 @@ export default class TestPage extends Vue {
 .header-text {
   position: relative;
   top: 40%;
+  text-shadow: 3px 3px 5px #2d2d2d;
 }
 .header-maintext {
   font-size: 44px;
