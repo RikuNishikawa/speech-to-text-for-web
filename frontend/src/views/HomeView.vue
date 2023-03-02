623ac8d08ded94f69d9ed60c75e14e9e625939eb
りく <nishi.riku09@gmail.com>
2023-03-02 11:45:56 +0900

    [change] test

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,4 +1,17 @@
 <template>
+  <div class="p-header">
+    <div class="container">
+      <p class="p-header_title">VCT</p>
+      <div class="p-header_nav">
+        <ul class="p-header_nav_list">
+          <li><a href="">menu</a></li>
+          <li><a href="">menu</a></li>
+          <li><a href="">menu</a></li>
+          <li><a href="">menu</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
   <hello-world />
 </template>
 
