7067b930414479dd26925c4d26761cec60c6a53c
りく <nishi.riku09@gmail.com>
2023-03-03 11:27:55 +0900

    [add] InputMainコンポーネント作成と文字の挿入

--- frontend/src/views/InputView.vue
+++ frontend/src/views/InputView.vue
@@ -2,6 +2,7 @@
   <v-app>
     <v-main>
       <header-main />
+      <input-main />
     </v-main>
   </v-app>
 </template>
@@ -10,11 +11,13 @@
 import { Options, Vue } from "vue-property-decorator";
 import TestPage from "@/components/TestPage.vue"; // @ is an alias to /src
 import HeaderMain from "@/components/HeaderMain.vue"; // @ is an alias to /src
+import InputMain from "@/components/InputMain.vue"; // @ is an alias to /src
 
 @Options({
   components: {
     TestPage,
     HeaderMain,
+    InputMain,
   },
 })
 export default class InputView extends Vue {}
