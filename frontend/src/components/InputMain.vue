3207ac1916a2c30b1a55542932bcc75e61fa2cd8
りく <nishi.riku09@gmail.com>
2023-03-03 18:20:54 +0900

    [update] 画像の挿入、変換コンポーネント(仮)、その他修正

--- frontend/src/components/InputMain.vue
+++ frontend/src/components/InputMain.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container class="input-home">
     <div class="input-home-main-pos">
-      <v-divider :thickness="3"></v-divider>
+      <v-divider :thickness="2"></v-divider>
       <div class="input-home-maintext font-weight-bold">
         <span>説</span><span>明</span>
       </div>
-      <v-divider :thickness="3"></v-divider>
+      <v-divider :thickness="2"></v-divider>
     </div>
     <div class="input-home-subtext">
       <ul>
@@ -13,6 +13,29 @@
         <li>音声データをフォルダから選択</li>
         <li>「変換」ボタンを押す</li>
       </ul>
+    </div>
+    <v-img
+      class="main-img"
+      :width="600"
+      aspect-ratio="16/9"
+      cover
+      src="../assets/2056912.png"
+    >
+    </v-img>
+    <!-- active -->
+    <div class="input-main-active">
+      <div
+        class="d-flex justify-space-around align-center flex-column flex-sm-row fill-height text-success"
+      >
+        <v-btn variant="falt" font-weight-bold>音声データを選択</v-btn>
+      </div>
+      <div
+        class="d-flex justify-space-around align-center flex-column flex-sm-row fill-height text-success"
+      >
+        <v-btn class="font-weight-bold" variant="falt" color="success"
+          >変換</v-btn
+        >
+      </div>
     </div>
   </v-container>
 </template>
@@ -38,15 +61,33 @@ export default class InputMain extends Vue {}
   padding-top: 48px;
   padding-bottom: 24px;
 }
+/* メインテキスト */
 .input-home-maintext {
   /* 48pxは大きすぎるかも */
-  font-size: 48px;
-  width: 145px;
+  font-size: 32px;
+  width: 220px;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
 }
+/* subtext */
 .input-home-subtext li {
   display: block;
-  padding-bottom: 10px;
+  padding-bottom: 8px;
+  letter-spacing: -0.1em;
+}
+/* img */
+.main-img {
+  margin: 0 auto;
+}
+
+/* active */
+.input-main-active {
+  display: flex;
+  justify-content: space-around;
+  width: 320px;
+  height: 40px;
+  margin: 0 auto;
+  border: 2px solid grey;
+  margin-top: 16px;
 }
 </style>
