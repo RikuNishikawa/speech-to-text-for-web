776a2cdacd02d9b448b72c79f182b0ccc906dade
りく <nishi.riku09@gmail.com>
2023-03-03 14:27:46 +0900

    [add] footer作成

---
+++ frontend/src/components/InputFooter.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-footer class="input-footer text-center d-flex flex-column">
+    <div>
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        :icon="icon"
+        variant="text"
+      ></v-btn>
+    </div>
+    <div class="pt-0 text-white">
+      Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris
+      cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id
+      ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui.
+      Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada.
+      Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin.
+      Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius
+      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+    </div>
+    <v-divider></v-divider>
+    <div class="text-white">
+      {{ new Date().getFullYear() }} — <strong>VCT</strong>
+    </div>
+  </v-footer>
+</template>
+<script lang="ts">
+import { Options, Vue } from "vue-property-decorator";
+
+@Options({})
+export default class TestPage extends Vue {
+  private extend = [".wav", ".mp3"];
+  private voiceFile = "";
+  public msg = "";
+  private onClick() {
+    alert(this.voiceFile);
+  }
+}
+</script>
+<style>
+.input-footer {
+  margin-top: 60px;
+  background-color: #303030;
+}
+</style>
