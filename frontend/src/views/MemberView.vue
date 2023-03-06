02ae09b35d62ec621a0238c783c856190ef15e83
りく <nishi.riku09@gmail.com>
2023-03-06 12:55:34 +0900

    [update] メンバー欄の挿入

--- frontend/src/views/MemberView.vue
+++ frontend/src/views/MemberView.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="font">
-    <input-header class="position" />
+  <div>
     <member />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-property-decorator";
-import InputHeader from "@/components/InputHeader.vue";
 import Member from "@/components/Member.vue"; // @ is an alias to /src
 
 @Options({
