1bf7a96bd1fe976aeb916a3332d44aca7ae2b018
ryoum-0930 <dara.ryouma@gmail.com>
2023-03-06 16:50:12 +0900

    [fix] member

--- frontend/src/components/Member.vue
+++ frontend/src/components/Member.vue
@@ -1,87 +1,66 @@
 <template>
-  <v-container>
-    <div class="member-title-pos my-8">
-      <v-divider class="border-opacity-50" :thickness="2"></v-divider>
-      <div class="member-title">開発メンバー紹介</div>
-      <v-divider class="border-opacity-50" :thickness="2"></v-divider>
-    </div>
-  </v-container>
-  <!-- img -->
-  <v-img class="member-img mb-8" src="../assets/member.png"></v-img>
+  <!-- <div>{{ memberInfo.name }}</div> -->
   <!-- member紹介 -->
-  <v-container>
-    <v-sheet>
-      <v-hover v-slot="{ isHovering, props }" open-delay="200">
-        <v-card
-          :elevation="isHovering ? 16 : 2"
-          :class="{ 'on-hover': isHovering }"
-          class="mx-0 member-01"
-          height="240"
-          max-width="350"
-          v-bind="props"
-        >
-          <v-card-text class="font-weight-normal ma-3">
-            <div class="member-name pb-10">石津龍真</div>
-            <div class="member-part pb-10">
-              <span>担当:</span><span class="color-01">全般サポート</span
-              ><br /><span>Email:</span><span class="color-01"></span>
-            </div>
-            <div class="member-sns"></div>
-          </v-card-text>
-        </v-card>
-      </v-hover>
-      <v-hover v-slot="{ isHovering, props }" open-delay="200">
-        <v-card
-          :elevation="isHovering ? 16 : 2"
-          :class="{ 'on-hover': isHovering }"
-          class="member-02"
-          height="240"
-          max-width="350"
-          v-bind="props"
-        >
-          <v-card-text class="font-weight-normal ma-3">
-            <div class="member-name pb-10">柴田康希</div>
-            <div class="member-part pb-10">
-              <span>担当:</span><span class="color-01">バックエンド</span
-              ><br /><span>Email:</span><span class="color-01"></span>
-            </div>
-            <div class="member-sns"></div>
-          </v-card-text>
-        </v-card>
-      </v-hover>
-      <v-hover v-slot="{ isHovering, props }" open-delay="200">
-        <v-card
-          :elevation="isHovering ? 16 : 2"
-          :class="{ 'on-hover': isHovering }"
-          class="mx-0 member-03"
-          height="240"
-          max-width="350"
-          v-bind="props"
-        >
-          <v-card-text class="font-weight-normal ma-3">
-            <div class="member-name pb-10">西川陸</div>
-            <div class="member-part pb-10">
-              <span>担当:</span
-              ><span class="color-01">デザイン/フロントエンド</span><br /><span
-                >Email:</span
-              ><span class="color-01"></span>
-            </div>
-            <div class="member-sns"></div>
-          </v-card-text>
-        </v-card>
-      </v-hover>
-    </v-sheet>
-  </v-container>
+  <!-- こんな感じでデータを取り出して一人分のパネルだけ作って、dataの中身はiconで隠して、iconクリック時のページ遷移にして欲しい -->
+  <!-- cardのホバーいる？アイコンとかにフォーカスしたときにクリック可能がどうかだけ分かる機能の方が欲しくない？ -->
+
+  <v-sheet>
+    <v-hover open-delay="200">
+      <v-card
+        :class="position"
+        class="mx-0 member-01"
+        height="240"
+        max-width="350"
+        v-bind="props"
+      >
+        <v-card-text class="font-weight-normal ma-3">
+          <div class="member-name pb-10">{{ memberInfo.name }}</div>
+          <div class="member-part pb-10">
+            <div>担当: {{ memberInfo.responsible }}</div>
+            <v-icon icon="mdi-github"></v-icon>
+            <v-icon icon="mdi-twitter"></v-icon>
+            <v-icon icon="mdi-facebook"></v-icon>
+            <span class="color-01"></span>
+          </div>
+          <div class="member-sns"></div>
+        </v-card-text>
+      </v-card>
+    </v-hover>
+  </v-sheet>
 </template>
 <script lang="ts">
-import { Options, Vue } from "vue-property-decorator";
+import { Options, Prop, Vue } from "vue-property-decorator";
+
+export interface URL {
+  Facebook: string;
+  Twitter: string;
+  GitHub: string;
+}
+
+export interface MemberInfo {
+  name: string;
+  responsible: string;
+  data: URL;
+  image: string;
+}
 
 @Options({})
-export default class Member extends Vue {}
+export default class Member extends Vue {
+  @Prop({ default: "" }) private memberInfo!: MemberInfo;
+  @Prop({ default: "right" }) private position!: string;
+}
 </script>
 <style scope>
 .color-01 {
   color: #459866;
+}
+.left {
+  display: flex;
+  justify-content: flex-end;
+}
+.rigth {
+  display: flex;
+  justify-content: flex-start;
 }
 .member-title-pos {
   display: flex;
