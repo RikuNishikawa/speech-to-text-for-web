35e646e835907c3dce2e48404e60837b0a62644f
ryoum-0930 <dara.ryouma@gmail.com>
2023-03-15 10:52:51 +0900

    [fix] conflict

--- backend_test/main.py
+++ backend_test/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
 
 
 @app.get("/api/{message}")
-def get_any_message(message: str):
+def get_any_message(message):
   return {"message": message}
 
 @app.post("/uploadtest/")
@@ -31,3 +31,37 @@ async def test(file: UploadFile = File(...)):
   print(contents)
   print("ok")
   return {"message": "success"}
+
+@app.post("/saveuploadfile/")
+async def save_upload_file_tmp(fileb: UploadFile=File(...)):
+  # tmp_path:Path = ""
+  print(fileb)
+  try:
+      # print(type(fileb))  # <class 'starlette.datastructures.UploadFile'>
+      # print(type(fileb.file)) #<class 'tempfile.SpooledTemporaryFile'>
+      suffix = Path(fileb.filename).suffix  # 拡張子の取得
+      path = f'./tmp/{fileb.filename}'
+      with open(path,'w+b') as buffer:
+        shutil.copyfileobj(fileb.file,buffer)
+        print("**********")
+        print(path,os.path.exists(path))
+        text = TransText(ModelType.BASE.value,path)
+        print("**********")
+      # tmpfile = NamedTemporaryFile(mode='r',suffix=suffix)
+      # with NamedTemporaryFile(delete=False, suffix=suffix) as tmpfile:
+      #     shutil.copyfileobj(fileb.file, tmpfile)
+      #     fileb.file.write()
+      #     print("tmpfile",tmpfile)
+      #     # tmpfile.write()
+      #     tmp_path = Path(tmpfile.name)
+      #     # print(tmp_path)
+  finally:
+      fileb.file.close()
+      #os.remove(path=path)
+  return {
+      "filename": fileb.filename,
+      "text":text,
+      "filesize": fileb.size,
+      # "temporary_filepath": tmp_path,
+      "fileb_content_type": fileb.content_type,
+  }
